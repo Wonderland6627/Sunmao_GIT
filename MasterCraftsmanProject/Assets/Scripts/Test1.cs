2f0f97295e946913736621cb7280613c2d466da5
Human <1194725964@qq.com>
2020-05-12 17:19:12 +0800

    HumanTest

---
+++ MasterCraftsmanProject/Assets/Scripts/Test1.cs
@@ -0,0 +1,20 @@
+/**
+*	Copyright(C) 2020 by DefaultCompany
+*	All rights reserved.
+*	FileName: Test1.cs
+*	Author: Human
+*	Version: 1.0
+*	UnityVersion：2018.4.16c1
+*	Date: 2020-05-12
+*	Description: Test
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test1 : MonoBehaviour 
+{
+
+	
+}
